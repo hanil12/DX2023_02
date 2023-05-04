1fb2e04bcc8cd5b3d8ed91f818263ddd7dc4d27a
hanil12 <98511268+hanil12@users.noreply.github.com>
2023-05-04 17:51:51 +0900

    Update DP_2.cpp

--- WIN_API/WIN_API_202302/Algorithm/DP_2.cpp
+++ WIN_API/WIN_API_202302/Algorithm/DP_2.cpp
@@ -18,7 +18,7 @@ using namespace std;
 // 1 2 5 (O)
 // 1 2 9 8 (X)
 
-vector<int> seq = {0,5,1,2,3,1};
+vector<int> seq = {1,2,3,4,3,4,5,6};
 
 // LIS(5) : 1
 // LIS(4) : LIS(5) or LIS(5) + 1
@@ -27,12 +27,72 @@ vector<int> seq = {0,5,1,2,3,1};
 // LIS(1) : LIS(2) or LIS(2) + 1
 // LIS(0) : LIS(1) or LIS(1) + 1 => 4
 
+vector<int> cache = vector<int>(100, -1);
+
 int LIS(int pos)
 {
-	return 0;
+	// 기저
+	if(pos == seq.size() - 1)
+		return 1;
+
+	// 캐싱
+	int& ref = cache[pos];
+
+	// 구하기
+	ref = 1;
+	for (int next = pos + 1; next < seq.size(); next++)
+	{
+		if (seq[pos] <= seq[next])
+		{
+			ref = max(ref, LIS(next) + 1);
+		}
+	}
+
+	return ref;
+}
+
+int myLis(int pos)
+{
+	if(pos == seq.size() - 1)
+		return 1;
+
+	int& ref = cache[pos];
+
+	if(ref != -1)
+		return ref;
+
+	int maxLis = 0;
+	int index = pos + 1;
+
+	for (int i = pos + 1; i < seq.size(); i++)
+	{
+		if (maxLis <= LIS(i))
+		{
+			if (seq[i] <= seq[index])
+			{
+				index = i;
+			}
+
+			maxLis = LIS(i);
+		}
+	}
+
+	if(seq[pos] < seq[index] || (index - pos) > maxLis)
+		ref = maxLis + 1;
+	else
+		ref = maxLis;
+
+	return ref;
 }
 
 int main()
 {
-	cout << LIS(0) << endl;
+	int lis = 0;
+
+	for (int i = 0; i < seq.size(); i++)
+	{
+		lis = max(lis, LIS(i));
+	}
+
+	cout << lis << endl;
 }
